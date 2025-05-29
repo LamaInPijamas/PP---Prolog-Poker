0f6258e6a4e108dc00404467803d39dc1bd1ac25
DAYNLIGHT (✿◡‿◡) <danielstodulski000@gmail.com>
2025-05-29 17:28:51 +0200

    Add players and make poker class CorePoker to add poker variants

--- Logic/Controller.py
+++ Logic/Controller.py
@@ -1,42 +1,59 @@
 from pyswip import Prolog
 
-# Player class 
+# ------------------ [ Player class ] ------------------ # 
 class Player:
   id = -1
-  def __init__(self, id):
+  nick = ""
+  points = 0
+  def __init__(self, id, properties, points = 0):
     self.id = id
+    self.nick = properties[0]
+    self.points = points
+  
+  def __str__(self):
+    return str(self.id) + ": " + self.nick + " " + str(self.points)
+  
+  def getInfo(self):
+    return [self.id, self.nick, self.points]
+  
 
-# Base Game initialization and actions in poker
-class Poker:
+# ------------------ [ Core Poker class ] ------------------ # 
+class CorePoker:
   prolog = Prolog()
   deck = []
+  players = []
   deal = []
   
-  def __init__(self, players=2):
-    if(2 > players or players > 10): 
+  def __init__(self, players):
+    if(2 > len(players) or len(players) > 10): 
       raise ValueError("Invalid numbers of players!!!")
 
+    # Set Players
+    for i in range(len(players)):
+      self.players.append(Player(i, players[i]))
+
+    # Initialized game
     self.prolog.consult("Main.pl")
     self.deck = list(self.prolog.query("create_deck(Deck)"))[0]['Deck']
-    deal = list(self.prolog.query(f"deal_cards({players}, {self.deck}, Deal, Rest)"))[0]
+    deal = list(self.prolog.query(f"deal_cards({len(players)}, {self.deck}, Deal, Rest)"))[0]
     self.deck = deal['Rest']
     self.deal = deal['Deal']
 
-  def getPlayerInfo(playerId):
-    pass
+  def __str__(self):
+    string = ""
+    for i in range(len(self.players)):
+      string += str(self.players[i]) + " " + str(self.deal[i]) + "\n"
+    string += str(self.deck)
+    return string
 
-  def check(self, playerId):
-    pass
-
-  def fold(self, playerId):
-    pass
-
-  def bet(self, playerId, amount):
-    pass
-
-
-
+  def getPlayersInfo(self):
+    table = []
+    for i in range(len(self.players)):
+      table.append([self.players[i].getInfo(), self.deal[i]])
+    return table
 
 
+# ------------------ [ Main ] ------------------ # 
+core = CorePoker([["Daniel"], ["Ruslan"]])
+print(core)
 
-game = Poker(2)
